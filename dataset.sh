22d3c85602b8398299f0fad11058c569341d37de
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2022-10-24 16:28:06 -0400

    starting condition for dataset.sh

--- dataset.sh
+++ dataset.sh
@@ -2,8 +2,9 @@
 # second argument is the number of shuffles
 # third argument is the number of polyforms
 # fourth argument is the prefix for where to place these files
+# fifth argument is the start offset
 
-for (( i=0; i<=$3; i++ ))
+for (( i=$5; i<=$5 + $3; i++ ))
 do
 	echo $i
 	./target/release/main --norender --length $1 --shuffles $2 --export analysis > $4/$i.txt
